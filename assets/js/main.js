90ae71999f179a1647574590016e038ca468481d
Moobbot <ngotam24082001@gmai.com>
2022-03-13 23:51:57 +0700

    fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,12 +12,12 @@
     /* 2. sticky And Scroll UP */
     $(window).on('scroll', function() {
         var scroll = $(window).scrollTop();
-        if (scroll < 100) {
+        if (scroll < 400) {
             $(".header-sticky").removeClass("sticky-bar");
-            $('#back-top').fadeOut(500);
+            $('#back-top').fadeOut(600);
         } else {
             $(".header-sticky").addClass("sticky-bar");
-            $('#back-top').fadeIn(500);
+            $('#back-top').fadeIn(600);
         }
     });
 
